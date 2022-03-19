d2ee44a96d7c0880001aa45305284e802ef15b48
Gabriela-Cleim <gabriela.cleims@gmail.com>
2022-03-19 18:20:39 -0300

    toolbar

--- app/src/main/java/com/gabrielacleim/unigames/DemoActivity.java
+++ app/src/main/java/com/gabrielacleim/unigames/DemoActivity.java
@@ -2,7 +2,10 @@ package com.gabrielacleim.unigames;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class DemoActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class DemoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo);
+
+        ImageView leftIcon = findViewById(R.id.left_icon_arrow);
+
+        leftIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
+            }
+        });
+
     }
 }
