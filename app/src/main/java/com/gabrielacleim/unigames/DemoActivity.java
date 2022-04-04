023b800f518511cb843442f6597be38116ab591d
Gabriela Cleim <gabriela.cleims@gmail.com>
2022-04-04 18:51:24 -0300

    função de video ok

--- app/src/main/java/com/gabrielacleim/unigames/DemoActivity.java
+++ app/src/main/java/com/gabrielacleim/unigames/DemoActivity.java
@@ -3,9 +3,12 @@ package com.gabrielacleim.unigames;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class DemoActivity extends AppCompatActivity {
 
@@ -16,12 +19,14 @@ public class DemoActivity extends AppCompatActivity {
 
         ImageView leftIcon = findViewById(R.id.left_icon_arrow);
 
-        leftIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),LoginActivity.class);
-            }
-        });
+        VideoView videoView = findViewById(R.id.video_view);
+        String videoPath = "android.resource://" + getPackageName() + "/" + R.raw.video;
+        Uri uri = Uri.parse(videoPath);
+        videoView.setVideoURI(uri);
+
+        MediaController mediaController = new MediaController(this);
+        videoView.setMediaController(mediaController);
+        mediaController.setAnchorView(videoView);
 
     }
 }
